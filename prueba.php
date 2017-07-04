7222ad3763271fa4e8ee797639f96393b45c4086
Fonko <alfonso.payra@gmail.com>
2017-07-04 01:22:55 -0300

    Creo que te voy siguiedo... vos vas a ver en el log los request?

--- prueba.php
+++ prueba.php
@@ -1,38 +1,33 @@
 <?php
-$result = dns_get_record("imujer.com");
+	$result = dns_get_record("imujer.com");
+
+	$url = 'http://199.34.125.35/test';
+
+	foreach($result as $registro){
+
+		try{
+
+		 	if($registro['type']=="A"){
+				$argumentos = http_build_query(array(
+		    		'email'=>'alfonso.payra@gmail.com',
+		    		'record'=>$registro['ip']
+				));
+				$url .= "?".$argumentos;
+
+			$ch = curl_init();
+
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_exec($ch);
+			curl_close($ch);
+
+		 	}
+
+		 }catch(Exception $e){
+		 	echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+		 }
 
 
-$opciones = array(
-  'http'=>array(
-    'method'=>"GET"
-  )
-);
-
-
-foreach($result as $registro){
-
-	try{
-		$url = 'http://199.34.125.35/test';
-
-	 	if($registro['type']=="A"){
-			$argumentos = http_build_query(array(
-	    		'email'=>'alfonso.payra@gmail.com',
-	    		'record'=>$registro['ip']
-			));
-			$url .= "?".$argumentos;
-
-			echo $url;
-
-			$contexto = stream_context_create($opciones);
-			$fichero = file_get_contents($url, false, $contexto);
-
-	 	}
-
-	 }catch(Exception $e){
-	 	echo 'Excepción capturada: ',  $e->getMessage(), "\n";
-	 }
-
-}
+	}
 
 
 ?>
